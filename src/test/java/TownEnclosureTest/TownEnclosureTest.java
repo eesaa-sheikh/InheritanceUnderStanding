9f98533f919fec9b77d5a2e4160024ade1c9f291
eesaasheikh <eesaasheikh3@gmail.com>
2022-11-22 10:13:50 +0000

    further improvements to the clases function

--- src/test/java/TownEnclosureTest/TownEnclosureTest.java
+++ src/test/java/TownEnclosureTest/TownEnclosureTest.java
@@ -28,12 +28,33 @@ public class TownEnclosureTest {
 
     //Testing to see if both subClasses
     public void canAddPropertyMansionandStudentFlat() {
-Mansion newMansion= new AbstractBuilding.Mansion("Shaquille O' Neals house", 32, 01.1796,true,12,29, 100);  //This needs to be fixed
-StudentFlat newStudentFlat= new AbstractBuilding.StudentFlat("Aston University", 12, 12.2009, 10, 10 ,false);
+Mansion newMansion= new AbstractBuilding.Mansion("Shaquille O' Neals house",
+        32,
+        01.1796,
+        true,
+        12,
+        29,
+        100);
+        //add a new studentFlat property
+StudentFlat newStudentFlat= new AbstractBuilding.StudentFlat("Aston University",
+        12,
+        12.2009,
+        10,
+        10 ,
+        false);
+    // New Mansion
+        Mansion belaAirMansion= new AbstractBuilding.Mansion("Uncle Phill's House",
+                8,
+                01.995,
+                false,
+                1,
+                2,
+                1);
 
-townEnclosure.addProperty(newMansion);
-townEnclosure.addProperty(newStudentFlat);
-    int expected = 2;
+        townEnclosure.addProperty(newMansion);
+        townEnclosure.addProperty(newStudentFlat);
+        townEnclosure.addProperty(belaAirMansion);
+    int expected = 3;
     int actual = townEnclosure.countProperty();
 
     assertEquals(expected, actual);
